268aaad17b0b994c8710b5345833e1d748a8cd47
Vova <vladymyr.pylypchatin@gmail.com>
2019-11-05 16:39:27 +0200

    fixes

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 puts "Publisihng on live server"
 
-eval spawn rsync -a -avz --progress  --exclude "/bookingsystem/node_modules" --exclude "/bookingsystem/source" --exclude ".git" . motilus:vova
+eval spawn rsync -a -avz --progress  --exclude "/bookingsystem/node_modules" --exclude "/bookingsystem/source" --exclude ".git" . motilus:vova2
 expect "password:"
 send "Motilhosting0!"
 send "\n"
